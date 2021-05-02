cda1657ef042e18a0e7c6936560081cfa8e683d8
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-05-02 20:17:31 +0200

    Start to work on moodle automator

---
+++ moodle/automator.py
@@ -0,0 +1,34 @@
+import logging
+
+from moodle.pages import LoginPage, ToggleEditPage
+from moodle.utility import config, get_driver
+
+logger = logging.getLogger(__name__)
+
+
+class Automator:
+    def __init__(self, *, wait_s: int = 3):
+        if wait_s <= 0:
+            msg = "Implicit wait must be positive!"
+            logger.error(msg)
+            raise ValueError(msg)
+
+        driver = get_driver()
+        driver.implicitly_wait(wait_s)
+        self.driver = driver
+
+        # execute login on moodle platform
+        self.login()
+
+        # enable course edit
+        self.enable_edit()
+
+    def login(self):
+        LoginPage(self.driver).complete()
+
+    def go_to_course(self):
+        self.driver.get(config["site"]["course"])
+
+    def enable_edit(self):
+        ToggleEditPage(self.driver).complete()
+        logger.info("Edit course enabled")
---
+++ moodle/pages.py
@@ -0,0 +1,59 @@
+import logging
+from abc import ABC
+
+from selenium.common import exceptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from moodle.utility import config
+
+logger = logging.getLogger(__name__)
+
+
+class Page(ABC):
+    """Abstract web page to complete"""
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def complete(self):
+        raise NotImplementedError
+
+
+class ToggleEditPage(Page):
+    def complete(self):
+        # assure we're on course page
+        self.driver.get(config["site"]["course"])
+
+        # click settings icon - gear
+        selector = "action-menu-toggle-2"
+        self.driver.find_element_by_id(selector).click()
+
+        selector = "#action-menu-2-menu > div:nth-child(2) > a"
+        self.driver.find_element_by_css_selector(selector).click()
+
+        logger.debug("Toggle editing of course done")
+
+
+class LoginPage(Page):
+    """Login page"""
+
+    def complete(self):
+        logger.info("Login started")
+
+        # go to login page
+        self.driver.get(config["site"]["login"])
+
+        try:
+            username_field = self.driver.find_element_by_id("username")
+            password_field = self.driver.find_element_by_id("password")
+        except exceptions.NoSuchElementException:
+            logger.warning("Was already logged in!")
+        except exceptions.WebDriverException as e:
+            logger.error(str(e))
+            raise e
+        else:
+            username_field.send_keys(config["credentials"]["username"])
+            password_field.send_keys(config["credentials"]["password"])
+            password_field.send_keys(Keys.ENTER)
+            logger.info("Logged in")
