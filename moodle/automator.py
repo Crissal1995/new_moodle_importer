2497300e437bc34b0b609f42635c8e5c2081a271
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-05-02 22:16:22 +0200

    Added creation of sections with name

--- moodle/automator.py
+++ moodle/automator.py
@@ -1,4 +1,7 @@
 import logging
+import time
+
+from selenium.webdriver.common.keys import Keys
 
 from moodle.pages import LoginPage, ToggleEditPage
 from moodle.utility import config, get_driver
@@ -7,6 +10,8 @@ logger = logging.getLogger(__name__)
 
 
 class Automator:
+    section_selector = "li[class^=section]"
+
     def __init__(self, *, wait_s: int = 3):
         if wait_s <= 0:
             msg = "Implicit wait must be positive!"
@@ -17,11 +22,19 @@ class Automator:
         driver.implicitly_wait(wait_s)
         self.driver = driver
 
+        # create counter for created sections
+        self.section_counter = 0
+        self.unnamed_section_counter = 0
+
         # execute login on moodle platform
         self.login()
 
         # enable course edit
         self.enable_edit()
+
+        # test create section
+        self.create_section("pippo baudo sei bello")
+        self.create_section()  # automatic name
 
     def login(self):
         LoginPage(self.driver).complete()
@@ -32,3 +45,54 @@ class Automator:
     def enable_edit(self):
         ToggleEditPage(self.driver).complete()
         logger.info("Edit course enabled")
+
+    def create_section(self, name: str = None) -> str:
+        """Create section and returns the text to find it again later"""
+        selector = "a[class=add-sections]"
+        self.driver.find_element_by_css_selector(selector).click()
+        time.sleep(0.5)
+        logger.debug("Clicked add-section button")
+
+        selector = "div[class=modal-footer] > button"
+        self.driver.find_element_by_css_selector(selector).click()
+        logger.info("Section created")
+
+        # the section created is the last one, so we'll take it
+        li = self.driver.find_elements_by_css_selector(self.section_selector)[-1]
+
+        # click the edit button, and then edit section
+        # TODO fixed italian
+        li.find_element_by_link_text("Modifica").click()
+        time.sleep(1)
+        li.find_element_by_link_text("Modifica argomento").click()
+
+        # another page will open...
+        time.sleep(1)
+
+        # then we need to set the section name
+        if not name:
+            self.unnamed_section_counter += 1
+            name = f"AUTOMATOR SECTION {self.unnamed_section_counter}"
+
+        logger.debug(f"name used: {name}")
+
+        input_name = self.driver.find_element_by_id("id_name_value")
+        disabled = input_name.get_property("disabled")
+
+        logger.info(f"input name, disabled value: {disabled}")
+
+        if disabled:
+            self.driver.find_element_by_id("id_name_customize").click()
+            logger.info("Checkbox was disabled, it was enabled")
+
+        time.sleep(1)
+        input_name.send_keys(name)
+        time.sleep(1)
+        input_name.send_keys(Keys.ENTER)
+
+        logger.info(f"Section renamed to '{name}'")
+
+        self.section_counter += 1
+        logger.debug(f"Created section number {self.section_counter}")
+
+        return name
